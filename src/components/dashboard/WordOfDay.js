b03c27ee61b0edd3ffb5e98ec170a5bab2de5401
ggderas19 <lmrderas@gmail.com>
2018-08-14 14:02:25 -0600

    Determining whether user has learned daily word

--- src/components/dashboard/WordOfDay.js
+++ src/components/dashboard/WordOfDay.js
@@ -3,36 +3,57 @@ import { connect } from 'react-redux';
 
 import { FetchRandomWord } from '../../words-api/WordsApi';
 import AppRouter, { history } from '../../routers/AppRouter';
+import moment from 'moment';
+import _ from 'underscore';
 
-class WordOfDay extends React.Component{
+class WordOfDay extends React.Component {
+
+    userHasAlreadyLearnedAWordToday() {
+        const { user } = this.props;
+        const today = moment().format("L");
+
+        return _.find(user.words || [], (w) => today === moment(w.learnedDate).format("L")) ? true : false;
+    }
+
+    goLearn() {
+        let { randomWord } = this.props;
+        history.push({
+            pathname: `/word/${randomWord.word}`,
+            state: randomWord
+        });
+    }    
+
     render() {
         let { randomWord } = this.props
 
         return (
             <div>
-                <div>
-                    <h1>{randomWord.word}</h1>
-                    <h3>Frequency {randomWord.frequency}</h3>
-                </div>            
+                {
+                    this.userHasAlreadyLearnedAWordToday() ? (
+                        <div>
+                            <h4>It looks you already learned a word today buddy!</h4>
+                        </div>
+                    ) : (
+                        <div>
+                            <div>
+                                <h1>{randomWord.word}</h1>
+                                <h3>Frequency {randomWord.frequency}</h3>
+                            </div>
 
-                <div>
-                    <button onClick={this.goLearn.bind(this)}>Go learn it!</button>
-                </div>
+                            <div>
+                                <button onClick={this.goLearn.bind(this)}>Go learn it!</button>
+                            </div>
+                        </div>
+                        )
+                }
             </div>
         )
-    }
-
-    goLearn(){
-        let { randomWord } = this.props;
-        history.push({
-            pathname: `/word/${randomWord.word}`,
-            state: randomWord
-        });        
     }
 }
 
 const mapStateToProps = (state, props) => ({
-    randomWord: state.randomWord
+    randomWord: state.randomWord,
+    user: state.user
 })
 
 export default connect(mapStateToProps, null)(WordOfDay);
